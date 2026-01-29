3f3a00e1c511bde5bdad69bff35dfba9750beca5
Ryan Miller <86577890+millerrc18@users.noreply.github.com>
2026-01-29 15:36:19 -0500

    Remove legacy program delivery helpers

--- app/models/program.rb
+++ app/models/program.rb
@@ -23,18 +23,4 @@ class Program < ApplicationRecord
   has_many :contracts, dependent: :destroy
   validates :name, presence: true
 
-  def total_due_units
-    contracts.sum { |c| c.total_due_units }
-  end
-
-  def on_time_units
-    contracts.sum { |c| c.on_time_units }
-  end
-
-  def on_time_rate
-    due = total_due_units
-    return 0.0 if due == 0
-    on_time_units.to_f / due
-  end
-
 end
